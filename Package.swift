4599a787886b25a7995628c736ff2e3aaa15b980
Christopher Cotton <me@christophercotton.com>
2023-11-01 15:13:55 -0500

    Updating SPM

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "IGListKit",
+    products: [
+        .library(name: "IGListKit", targets: ["IGListKit"])
+    ],
+    targets: [
+        .target(
+            name: "IGListKit",
+            path: "Source"
+        )
+    ]
+)
